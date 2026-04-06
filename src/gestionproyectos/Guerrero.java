20885a4868702319b36cc41fbfe61fbcdd15184e
Andres Felipe Noy Moreno <afnoym@udistrital.edu.co>
2026-04-06 16:39:15 -0500

    feat: agrega clase guerrero (herencia de tarea)

---
+++ src/gestionproyectos/Guerrero.java
@@ -0,0 +1,14 @@
+package gestionproyectos;
+
+// La clase Guerrero hereda todos los atributos y métodos de Tarea
+public class Guerrero extends Tarea {
+    
+    public Guerrero() {
+        // Aquí puedes inicializar propiedades específicas del Guerrero
+    }
+
+    @Override
+    public void ejecutarTarea() {
+        
+    }
+}
