2038ce9819a297d200e1314e31974d11afcd7aaf
Andres Felipe Noy Moreno <afnoym@udistrital.edu.co>
2026-04-06 16:20:35 -0500

    feat: agrega metodo calcularAvance

--- src/gestionproyectos/Proyecto.java
+++ src/gestionproyectos/Proyecto.java
@@ -1,7 +1,15 @@
 package gestionproyectos;
 
 public class Proyecto {
+    
+    // Este es tu constructor actual (déjalo como está)
     public Proyecto(){
-        
+
     }
-}
+
+    // PEGA EL MÉTODO AQUÍ (dentro de la clase, abajo del constructor)
+    public void calcularAvance() {
+        System.out.println("Calculando el avance del proyecto...");
+    }
+
+} // Esta es la llave que cierra la clase. Asegúrate de que no falte.
